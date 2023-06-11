85fc11f4a376f680e994776f0d4f9e26cc7e40f0
Katy Douglas <98225667+K-doug@users.noreply.github.com>
2023-06-11 18:30:58 +0100

    Add files via upload

---
+++ Magic 8-Ball.js
@@ -0,0 +1,35 @@
+let randomNumber = Math.random();
+
+randomNumber = randomNumber * 6;
+
+randomNumber = Math.floor(randomNumber);
+
+console.log(randomNumber);
+
+let user = prompt("What is your question?");
+
+let message = "";
+
+switch (randomNumber){
+    case 0:
+        message = "Ask again later";
+        break;
+    case 1:
+        message = "Don't count on it";
+        break;
+    case 2: 
+        message = "My reply is no";
+        break;
+    case 3:
+        message = "it looks promising";
+        break;
+    case 4:
+        message = "it is certain";
+        break;
+    case 5: 
+        message = "signs point to yes";
+        break;
+}
+
+console.log(user);
+console.log(message);
